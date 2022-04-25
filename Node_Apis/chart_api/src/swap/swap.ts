4c3f4c60e25a6a78fbd2fafdf4d1fc907ce692ba
govindumat <govind.umat@imentus.com>
2022-04-25 12:44:15 +0530

    update swap api

--- Node_Apis/chart_api/src/swap/swap.ts
+++ Node_Apis/chart_api/src/swap/swap.ts
@@ -1,11 +1,12 @@
-const SwapTable = require("./swapHistory.entity");
+import { SwapTable } from "./swapHistory.entity";
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const Web3 = require("web3");
 const Swap_ABI = require("./swapContract");
 const Provider = require("@truffle/hdwallet-provider");
-const getRepository = require("typeorm");
+// const getRepository = require("typeorm");
+import { Connection, getRepository, Transaction,createQueryBuilder, TransactionRepository, Index } from "typeorm";
 const privateKey ='ee7615b734368bcc3d901c76fb8882f0072ffc9b26510f2ecb062e02db386775';
 const provider = new Provider(privateKey, "https://testnet.dexit.network");
 const web3 = new Web3(provider);
@@ -67,17 +68,23 @@ export const claimETH = async (from:string,amount:bigint,exc_rate:number,transac
   if(hash_value == transactionHash) {
       // console.log("\nTransaction already processed");
       status = 'error';
+      return{status};
   }
   if(network == 'BNB'){
      transactionDetails = await web3BSC.eth.getTransactionReceipt(transactionHash);
-  }else{
+  }else if(network == 'DXT'){
      transactionDetails = await web3DXT.eth.getTransactionReceipt(transactionHash);
+  }else{
+    status = 'Unknown network';
+    return{status};
   }
+  
   
   // console.log("Checking credentials for transactionDetails");
 
   if(transactionDetails == null) {
       status = 'invalid hash';
+      return{status};
   }
 
   // console.log("transactionDetails.from : ",transactionDetails.from );
@@ -100,7 +107,7 @@ export const claimETH = async (from:string,amount:bigint,exc_rate:number,transac
   console.log("status is : ", status);
 
   const txRepo = getRepository(SwapTable);
-  await txRepo.insert({from:from,amount:amount,exc_rate:exc_rate,transactionHash:transactionHash,network:network,tx_status:status})
+  await txRepo.insert({from:from,amount:amount.toString(),exc_rate:exc_rate,transactionHash:transactionHash,network:network,tx_status:status})
   
  if(status == '200'){
   console.log("again status is : ", status);
@@ -110,9 +117,10 @@ export const claimETH = async (from:string,amount:bigint,exc_rate:number,transac
       let result = await tokenETH.methods.withdraw(from,amount,exc_rate,transactionHash).send({
         from : accounts[0]
     });
+
     console.log("result ", result);
       console.log("BSC withdraw successfully");
-      
+      return{status, result};
     } catch (error) {
       console.log(error);
     }
@@ -169,7 +177,7 @@ export const claimBSC = async (from:string,amount:bigint,exc_rate:number,transac
   console.log("status is : ", status);
   
   const txRepo = getRepository(SwapTable);
-  await txRepo.insert({from:from,amount:amount,exc_rate:exc_rate,transactionHash:transactionHash,network:network,tx_status:status})
+  await txRepo.insert({from:from,amount:amount.toString(),exc_rate:exc_rate,transactionHash:transactionHash,network:network,tx_status:status})
 
  if(status == '200'){
   // console.log("again status is : ", status);
@@ -229,7 +237,7 @@ export const claimDXT = async (from:string,amount:bigint,exc_rate:number,transac
     if (transactionDetails.from == from.toLowerCase() && transactionDetails.to == contractAddrBSC.toLowerCase() && transactionDetails.status == true) {     // Needs to be converted to lower case since Metamasks check sum requires some letters to be capital and addresses returned from transaction hash are in lowercase
       status = '200';
     }
-    else if (transactionDetails.from == from.toLowerCase() && transactionDetails.to == contractAddrDXT.toLowerCase() && transactionDetails.status == true) {     // Needs to be converted to lower case since Metamasks check sum requires some letters to be capital and addresses returned from transaction hash are in lowercase
+    else if (transactionDetails.from == from.toLowerCase() && transactionDetails.to == contractAddrETH.toLowerCase() && transactionDetails.status == true) {     // Needs to be converted to lower case since Metamasks check sum requires some letters to be capital and addresses returned from transaction hash are in lowercase
       status = '200';
     }
     else{
@@ -239,7 +247,7 @@ export const claimDXT = async (from:string,amount:bigint,exc_rate:number,transac
     console.log("status is : ", status);
     
     const txRepo = getRepository(SwapTable);
-    await txRepo.insert({from:from,amount:amount,exc_rate:exc_rate,transactionHash:transactionHash,network:network,tx_status:status})
+    await txRepo.insert({from:from,amount:amount.toString(),exc_rate:exc_rate,transactionHash:transactionHash,network:network,tx_status:status})
 
    if(status == '200'){
     // console.log("again status is : ", status);
@@ -256,5 +264,6 @@ export const claimDXT = async (from:string,amount:bigint,exc_rate:number,transac
         console.log(error);
       }
     }
+    
 
 }
